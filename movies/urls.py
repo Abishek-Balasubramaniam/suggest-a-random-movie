d05044f2327858ceed77690f3fb409022cb57927
unknown <2903abi@gmail.com>
2020-05-14 00:15:37 +0530

    first commit

--- movies/urls.py
+++ movies/urls.py
@@ -2,8 +2,10 @@ from django.urls import re_path
 from django.contrib import admin
 from django.urls import path
 from . import views
-app_name="movies"
+
+app_name = 'movies'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^$',views.movies,name="random_movie")
+    re_path(r'^$',views.movie_detail,name='random_movie')
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from justwatch import JustWatch
 import imdb
 import random
-def movies(request):
+def movie_detail(request):
     moviesdb=imdb.IMDb()
     top=moviesdb.get_top250_movies()
+    just_watch = JustWatch(country='IN')
     #id_list=[id for id in top[i].getID for i in range(0,250)]
     my_movie=random.choice(top)
+    results = just_watch.search_for_item(query=my_movie['title'])
+    list = results['items']
+    url=[]
+    try:
+        for item in list[0]['offers']:
+            path=item['urls']['standard_web'].split('.')
+            if "netflix" in path[1]:
+                url.append('Netflix')
+            elif "amazon" in path[1]:
+                url.append('Amazon prime video')
+            elif "prime" in path[1]:
+                url.append('Amazon prime video')
+            elif "hotstar" in path[1]:
+                url.append('Disney+ Hotstar')
+            elif "disney" in path[1]:
+                url.append('Disney+ Hotstar')
+            elif "apple" in path[1]:
+                url.append('Apple itunes')
+            elif "google" in path[1]:
+                url.append('Rent from play-store')
+    except KeyError:
+        url.append("Couldn't find a streaming service")
+    if len(url) == 0:
+         url.append("Couldn't find a streaming service")
+    else:
+        my_movie['streaming_platforms']=set(url)
+    year=my_movie['year']
+    tags=(my_movie['title'].lower().split(' '))
+    slug=""
+    for tag in tags:
+        if tag != ':':
+            slug+=tag+'-'
+
+    my_movie['free_url']="https://ww5.fmovie.sc/online/"+slug+str(year)
     movie=moviesdb.get_movie(my_movie.movieID)
     my_movie.genre=movie['genre']
     plot=movie['plot'][0].split('::')
