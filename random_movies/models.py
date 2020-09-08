028962148ef66d2b558a9ccced9e88fc74025455
Abishek-Balasubramaniam <65147392+Abishek-Balasubramaniam@users.noreply.github.com>
2020-09-08 20:56:11 +0530

    Update models.py

--- random_movies/models.py
+++ random_movies/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 class Movie(models.Model):
     title=models.CharField(max_length=2500)
     url=models.CharField(max_length=2500)
-    avail_on=models.CharField(max_length=2500)
+    #avail_on=models.CharField(max_length=2500)
     author=models.ForeignKey(User,default=None,on_delete=models.CASCADE)
     def __str__(self):
         return self.title
